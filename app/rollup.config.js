ac82d698ccd7252ada9f66513ce5692d4ded69dd
deploy.sh <ellemenno@users.noreply.github.com>
2021-02-16 12:38:36 -0500

    clean up dependencies and simplify build scripts

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -1,28 +1,11 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
-const production = !process.env.ROLLUP_WATCH; // based on --watch flag, absent from `npm run build`, present in `npm run start`
-
-const serve = () => {
-  let server;
-  function toExit() { if (server) server.kill(0); }
-
-  return {
-    writeBundle() {
-      if (server) return;
-      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-        stdio: ['ignore', 'inherit', 'inherit'],
-        shell: true
-      });
-
-      process.on('SIGTERM', toExit);
-      process.on('exit', toExit);
-    }
-  };
-}
+const build_for_dev = process.env.BUILD_FOR_DEV && process.env.BUILD_FOR_DEV === 'true';
+const build_for_prod = !build_for_dev;
+console.log(`building a ${build_for_dev ? 'dev' : 'prod'} release`);
 
 const std_output = (module_name) => ({
   file: `public/build/${module_name}.js`,
@@ -34,12 +17,10 @@ const std_output = (module_name) => ({
 const get_plugins = (module_name) => {
   // start with the standard fare..
   const plugins = [
-    svelte({ compilerOptions: { dev: !production } }), // enable run-time checks when not in production
+    svelte({ compilerOptions: { dev: build_for_dev } }), // enable run-time checks when building for dev
     resolve({ browser: true, dedupe: ['svelte'] }), // resolve and dedupe 3rd party modules in ./node_modules
     css({ output: `${module_name}.css` }), // extract component CSS into separate file for better performance
-    !production && serve(), // start server if not in production mode
-    !production && livereload('public'), // watch the `public` directory and rebuild on changes
-    production && terser(), // in production mode, minify to reduce file size
+    build_for_prod && terser(), // in production mode, minify to reduce file size
   ];
 
   return plugins;
