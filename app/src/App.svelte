9c54ce0ca66995067a44edff4548e604f03f675a
deploy.sh <ellemenno@users.noreply.github.com>
2021-02-16 16:37:37 -0500

    add to-scale clickable court with coordinate space conversions

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+  import Recorder from "./Recorder.svelte";
+  import Visualizer from "./Visualizer.svelte";
+  import Tabs from "./Tabs.svelte";
+
+  let items = [ // provide labels, values and assigned components
+    { label: "Record",    value: 1, component: Recorder   },
+    { label: "Visualize", value: 2, component: Visualizer },
+  ];
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>vbstats</h1>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Tabs {items} />
